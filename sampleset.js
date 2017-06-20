0138230a6975af2423b22aaabcca6adc4ffd9e9a
Graham Nott <gnott@starglobal.ca>
2017-06-20 16:47:10 -0700

    Update XML source file examples to be up-to-date.

--- sampleset.js
+++ sampleset.js
@@ -3,15 +3,15 @@ module.exports = [
     "name": "eLife Samples",
     "samples": [
       {
-        "url": "https://s3.amazonaws.com/elife-publishing-cdn/00311/elife-00311-v1.xml",
+        "url": "https://cdn.elifesciences.org/articles/00311/elife-00311-v1.xml",
         "comment": ""
       },
       {
-        "url": "https://s3.amazonaws.com/elife-publishing-cdn/00778/elife-00778-v1.xml",
+        "url": "https://cdn.elifesciences.org/articles/00778/elife-00778-v1.xml",
         "comment": ""
       },
       {
-        "url": "https://s3.amazonaws.com/elife-publishing-cdn/15278/elife-15278-v1.xml",
+        "url": "https://cdn.elifesciences.org/articles/15278/elife-15278-v1.xml",
         "comment": ""
       },
       {
